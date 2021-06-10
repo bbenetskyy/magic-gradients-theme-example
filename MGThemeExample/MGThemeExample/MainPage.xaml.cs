98a9e5e5ca69f136d154c4c05e660403c55b93ce
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2021-06-10 07:39:33 +0200

    Dynamic App Theme Magic Gradients

---
+++ MGThemeExample/MGThemeExample/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MGThemeExample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+            Application.Current.RequestedThemeChanged += (s, a) =>
+            {
+                GradientView.StyleClass = new[]
+                {
+                    a.RequestedTheme == OSAppTheme.Light
+                        ? "lightGradient"
+                        : "darkGradient"
+                };
+            };
+        }
+    }
+}
